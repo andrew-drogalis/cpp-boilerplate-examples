16d28c70645d7d480600e147b8a578063ad676f3
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-02-24 17:09:43 -0500

    Added exceptions

---
+++ exceptions/main.cpp
@@ -0,0 +1,37 @@
+#include <stdexcept>
+
+
+// logic_error
+// exception class to indicate violations of logical preconditions or class invariants
+
+// invalid_argument
+// exception class to report invalid arguments
+
+// domain_error
+// exception class to report domain errors
+
+// length_error
+// exception class to report attempts to exceed maximum allowed size
+
+// out_of_range
+// exception class to report arguments outside of expected range
+
+// runtime_error
+// exception class to indicate conditions only detectable at run time
+
+// range_error
+// exception class to report range errors in internal computations
+
+// overflow_error
+// exception class to report arithmetic overflows
+
+// underflow_error
+// exception class to report arithmetic underflows
+
+
+int main () {
+
+
+
+  return 0;
+}
